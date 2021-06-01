b592396a4ec3c57bb2225f187ae4594c9143c9e2
Emmanuel Akozi <akoziemmanuel@gmail.com>
2021-06-01 02:25:14 +0100

    Progress bar and buttons fixed

--- app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
+++ app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
@@ -3,11 +3,13 @@ package com.decagonhq.clads
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed(
@@ -16,7 +18,7 @@ class SplashScreenActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             },
-            1000
+            1500
         )
     }
 }

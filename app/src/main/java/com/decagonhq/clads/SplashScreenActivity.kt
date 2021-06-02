7d4f26a5e7d52709c271da83119285c82058e63e
Emmanuel Akozi <akoziemmanuel@gmail.com>
2021-06-02 06:33:47 +0100

    Corrected wrong naming convention

--- app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
+++ app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.decagonhq.clads.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+    private var _binding: ActivitySplashScreenBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_splash_screen)
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler().postDelayed(
             {

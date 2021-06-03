f0b1d8f0a53f0be578e26cea659088307ac44dc4
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-06-03 15:09:13 +0100

    Resolved code style violation due to wildcard import

--- app/src/androidTest/java/com/decagonhq/clads/SignUpOptionsFragmentUITest.kt
+++ app/src/androidTest/java/com/decagonhq/clads/SignUpOptionsFragmentUITest.kt
@@ -6,7 +6,10 @@ import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith

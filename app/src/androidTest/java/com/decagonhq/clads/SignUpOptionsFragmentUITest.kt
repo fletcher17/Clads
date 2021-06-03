43a774c8943268214964a66ebf51064a91916d78
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-06-03 15:15:09 +0100

    Resolved KtLintCheck

--- app/src/androidTest/java/com/decagonhq/clads/SignUpOptionsFragmentUITest.kt
+++ app/src/androidTest/java/com/decagonhq/clads/SignUpOptionsFragmentUITest.kt
@@ -6,8 +6,8 @@ import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4

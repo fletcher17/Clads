a6f80b4291890ee857823c84227597879358d278
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-06-07 14:33:53 +0100

    viewPager and tabLayout created

--- app/src/main/java/com/decagonhq/clads/ui/view/activity/ProfileDashboardActivity.kt
+++ app/src/main/java/com/decagonhq/clads/ui/view/activity/ProfileDashboardActivity.kt
@@ -2,11 +2,13 @@ package com.decagonhq.clads.ui.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.decagonhq.clads.R
 import com.decagonhq.clads.databinding.ActivityProfileDashboardBinding
 
 class ProfileDashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(ActivityProfileDashboardBinding.inflate(layoutInflater).root)
+        supportActionBar?.hide()
     }
 }

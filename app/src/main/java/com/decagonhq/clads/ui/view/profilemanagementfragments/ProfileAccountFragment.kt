b4f206082006b728a5d81758e29decb2de1c06ad
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-06-08 14:35:43 +0100

    Added gender dialog fragment

--- app/src/main/java/com/decagonhq/clads/ui/view/profilemanagementfragments/ProfileAccountFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/view/profilemanagementfragments/ProfileAccountFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.decagonhq.clads.R
 import com.decagonhq.clads.databinding.FragmentProfileAccountTabBinding
 import com.decagonhq.clads.ui.view.profilemanagementfragments.dialogfragments.*
 
@@ -25,39 +26,69 @@ class ProfileAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.fragmentProfileAccountFirstNameEditText.setOnClickListener {
             val dialog = EditProfileAccountFirstNameCustomDialogFragment()
-            dialog.show(childFragmentManager, "firstNameDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_first_name_dialog_fragment_tag)
+            )
         }
 
         binding.fragmentProfileAccountLastNameEditText.setOnClickListener {
             val dialog = EditProfileAccountLastNameCustomDialogFragment()
-            dialog.show(childFragmentManager, "lastNameDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_last_name_dialog_fragment_tag)
+            )
         }
 
         binding.fragmentProfileAccountOtherNameEditText.setOnClickListener {
             val dialog = EditProfileAccountOtherNameCustomDialogFragment()
-            dialog.show(childFragmentManager, "otherNameDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_other_name_dialog_fragment_tag)
+            )
         }
 
         binding.fragmentProfileAccountShowroomAddressEditText.setOnClickListener {
             val dialog = EditProfileAccountShowroomAddressCustomDialogFragment()
-            dialog.show(childFragmentManager, "showroomDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_showroom_address_dialog_fragment_tag)
+            )
         }
 
         binding.fragmentProfileAccountWorkshopAddressEditText.setOnClickListener {
             val dialog = EditProfileAccountWorkshopAddressCustomDialogFragment()
-            dialog.show(childFragmentManager, "workshopDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_workshop_address_dialog_fragment_tag)
+            )
         }
 
         binding.fragmentProfileAccountNumberOfEmployeesEditText.setOnClickListener {
             val dialog = EditProfileAccountNumberOfEmployeesCustomDialogFragment()
-            dialog.show(childFragmentManager, "numberOfEmployeesDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_number_of_employees_dialog_fragment_tag)
+            )
         }
 
         binding.fragmentProfileAccountLegalStatusEditText.setOnClickListener {
             val dialog = EditProfileAccountLegalStatusCustomDialogFragment()
-            dialog.show(childFragmentManager, "legalStatusDialog")
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_legal_status_dialog_fragment_tag)
+            )
+        }
+
+        binding.fragmentProfileAccountGenderEditText.setOnClickListener {
+            val dialog = EditProfileAccountGenderCustomDialogFragment()
+            dialog.show(
+                childFragmentManager,
+                getString(R.string.profile_account_fragment_gender_dialog_fragment_tag)
+            )
         }
 
     }
--- app/src/main/java/com/decagonhq/clads/ui/view/profilemanagementfragments/dialogfragments/EditProfileAccountGenderCustomDialogFragment.kt
+++ app/src/main/java/com/decagonhq/clads/ui/view/profilemanagementfragments/dialogfragments/EditProfileAccountGenderCustomDialogFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.decagonhq.clads.databinding.FragmentProfileAccountFirstNameCustomDialogBinding
+import com.decagonhq.clads.databinding.FragmentProfileAccountGenderCustomDialogBinding
 import com.decagonhq.clads.databinding.FragmentProfileAccountLastNameCustomDialogBinding
 
 class EditProfileAccountGenderCustomDialogFragment : DialogFragment() {
 
-    private var _binding: FragmentProfileAccountLastNameCustomDialogBinding? = null
+    private var _binding: FragmentProfileAccountGenderCustomDialogBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -19,18 +20,19 @@ class EditProfileAccountGenderCustomDialogFragment : DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding =
-            FragmentProfileAccountLastNameCustomDialogBinding.inflate(inflater, container, false)
 
-        binding.fragmentProfileAccountLastNameCustomDialogCancelTextView.setOnClickListener {
+        _binding =
+            FragmentProfileAccountGenderCustomDialogBinding.inflate(inflater, container, false)
+
+        binding.femaleRadioButton.setOnClickListener {
+            val selectedGender = binding.femaleRadioButton.text.toString()
+            Toast.makeText(context, "Wow! You are a $selectedGender?", Toast.LENGTH_LONG).show()
             dismiss()
         }
 
-        binding.fragmentProfileAccountLastNameCustomDialogOkTextView.setOnClickListener {
-            val lastName =
-                binding.fragmentProfileAccountLastNameCustomDialogLastNameEditText.editText?.text.toString()
-            Toast.makeText(context, "Is your last Name really $lastName ?", Toast.LENGTH_LONG)
-                .show()
+        binding.maleRadioButton.setOnClickListener {
+            val selectedGender = binding.maleRadioButton.text.toString()
+            Toast.makeText(context, "We don't do $selectedGender here!", Toast.LENGTH_LONG).show()
             dismiss()
         }
 
